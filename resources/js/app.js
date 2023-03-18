fc0316a6b9d0df7f8e89517f9e559aa61904228a
menene <efmarroquinr@gmail.com>
2023-03-17 20:06:53 -0600

    agregar auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,31 +4,19 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import "./bootstrap";
+import './bootstrap';
+import { createApp } from 'vue';
 
-import Vue from 'vue';
-// import VueSweetalert2 from 'vue-sweetalert2';
+/**
+ * Next, we will create a fresh Vue application instance. You may then begin
+ * registering components with the application instance so they are ready
+ * to use in your application's views. An example is included for you.
+ */
 
-// If you don't need the styles, do not connect
-// import 'sweetalert2/dist/sweetalert2.min.css';
+const app = createApp({});
 
-// Vue.use(VueSweetalert2);
-
-// Vue.filter('fecha', value => moment(value).format('DD/MM/YYYY'))
-// Vue.filter('fechaMysql0', value => moment(value).format('YYYY-MM-DD 00:00:00'))
-// Vue.filter('fechaMysql23', value => moment(value).format('YYYY-MM-DD 23:59:59'))
-// Vue.filter('fechaHora', value => moment(value).format('DD/MM/YYYY HH:mm'))
-// Vue.filter('fechaHoraCorta', value => moment(value).format('DD/MM HH:mm'))
-
-// import CatalogsRolesEdit from '../../vendor/csgt/utils/src/resources/views/catalogs/RolesEdit.vue';
-// import CatalogsRolemodule from '../../vendor/csgt/utils/src/resources/views/catalogs/RoleModule.vue';
-// import CatalogsUsersEdit from '../../vendor/csgt/utils/src/resources/views/catalogs/UsersEdit.vue';
-// import Profile from '../../vendor/csgt/utils/src/resources/views/Profile.vue';
-// import InputField from '../../vendor/csgt/utils/src/resources/components/InputField.vue';
-
-// import Equipment from './views/Catalogs/Equipment.vue';
-
-window.Vue = Vue;
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
 
 /**
  * The following block of code may be used to automatically register your
@@ -38,27 +26,14 @@ window.Vue = Vue;
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context("./", true, /\.vue$/i);
-// files
-//     .keys()
-//     .map((key) =>
-//         Vue.component(key.split("/").pop().split(".")[0], files(key).default)re  
-//     );
-
-// Vue.component('catalogs-roles-edit', CatalogsRolesEdit);
-// Vue.component('catalogs-rolemodule', CatalogsRolemodule);
-// Vue.component('catalogs-users-edit', CatalogsUsersEdit);
-// Vue.component('profile', Profile);
-// Vue.component('InputField', InputField);
-
-// Vue.component('Equipment', Equipment);
+// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+// });
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Finally, we will attach the application instance to a HTML element with
+ * an "id" attribute of "app". This element is included with the "auth"
+ * scaffolding. Otherwise, you will need to add an element yourself.
  */
 
-const app = new Vue({
-    el: "#app",
-});
+app.mount('#app');
